89adbb9423ad72034e8d72a10523b0949aa71559
msenior526 <msenior526@gmail.com>
2021-05-23 14:08:09 -0400

    commit tree a79c78b6635ba3be162f525bf6ad4f6f76515873

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Add your functions here
+const map = (array, callback) => {
+    const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+        let el = array[i];
+        debugger
+        newArr.push(callback(array));
+    }
+    return newArr;
+}
